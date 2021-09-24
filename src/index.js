a3932a150fc4fbe249812b04cdbdd31c1189cae1
Onkar Swami <onkarswami2002@gmail.com>
2021-09-24 23:31:13 +0530

    single page application is ready

--- src/index.js
+++ src/index.js
@@ -4,48 +4,27 @@ import Card from './Card';
 import sdata from './sdata';
 import './index.css';
 
+
 ReactDOM.render(
   <>
     <h1 className="heading_style">TOP 5 NETFLIX SERIES</h1>
-    <div class="wrapper">
-                <div class="cards_wrap">
-    <Card 
-      imgsrc = {sdata[0].imgsrc}
-      title  = {sdata[0].title}
-      sname  = {sdata[0].sname}
-      link   = {sdata[0].link}
-    />
+    
+    <div className="wrapper">
+      <div className="cards_wrap">
 
-    <Card 
-      imgsrc = {sdata[1].imgsrc}
-      title  = {sdata[1].title}
-      sname  = {sdata[1].sname}
-      link   = {sdata[1].link}
-    />
-
-    <Card 
-      imgsrc = {sdata[2].imgsrc}
-      title  = {sdata[2].title}
-      sname  = {sdata[2].sname}
-      link   = {sdata[2].link}
-    />
-
-    <Card 
-      imgsrc = {sdata[3].imgsrc}
-      title  = {sdata[3].title}
-      sname  = {sdata[3].sname}
-      link   = {sdata[3].link}
-    />
-
-    <Card 
-      imgsrc = {sdata[4].imgsrc}
-      title  = {sdata[4].title}
-      sname  = {sdata[4].sname}
-      link   = {sdata[4].link}
-    />
-
+        {sdata.map((val) => {
+          return (  <Card
+              imgsrc={val.imgsrc}
+              title={val.title}
+              sname={val.sname}
+              link={val.link}
+            />
+          );
+        })}
+        
+      </div>
     </div>
-    </div>
+
   </>,
   document.getElementById('root')
 );
